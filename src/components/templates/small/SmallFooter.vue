b517b790f84fd7c269f61c4b5ce6303c69bc8756
Guilherme Passos <gepassos06@gmail.com>
2024-07-09 11:07:30 -0300

    ajustes finos na organização do projeto

--- src/components/templates/small/SmallFooter.vue
+++ src/components/templates/small/SmallFooter.vue
@@ -1,3 +1,67 @@
+<script setup>
+import AccountCircleOutline from 'vue-material-design-icons/AccountCircleOutline.vue';
+import CartOutline from 'vue-material-design-icons/CartOutline.vue';
+import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
+import Menu from 'vue-material-design-icons/Menu.vue';
+</script>
 <template>
-    <h2>Small Footer</h2>
-  </template>
+  <div class="icons">
+    <RouterLink to="/">
+      <HomeOutline size="25" fillColor="#282828" />
+      Home
+    </RouterLink>
+    <RouterLink to="/">
+      <AccountCircleOutline size="25" fillColor="#282828" />
+      Perfil
+    </RouterLink>
+    <RouterLink to="/">
+      <CartOutline size="25" fillColor="#282828" />
+      Carrinho
+    </RouterLink>
+    <RouterLink to="/">
+      <Menu size="25" fillColor="#282828" />
+      Menu
+    </RouterLink>
+  </div>
+</template>
+<style scoped>
+#footerMenu {
+  position: fixed;
+  bottom: 15%;
+  right: 0;
+
+  width: 20%;
+  border-top: #eeeeee 1px solid;
+  background-color: white;
+
+  display: block;
+  padding: 1rem;
+}
+
+#footerMenu a {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  text-decoration: none;
+  color: #282828;
+  font-size: 1rem;
+  transition: color 0.3s;
+}
+
+.icons {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.icons a {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
+  color: #282828;
+  font-size: 1rem;
+  transition: color 0.3s;
+}
+</style>
--- src/layouts/LayoutLarge.vue
+++ src/layouts/LayoutLarge.vue
@@ -19,54 +19,51 @@ import LargeFooter from '@/components/templates/large/LargeFooter.vue'
     </footer>
   </div>
 </template>
+
 <style scoped>
-#layout-small {
+
+#layout-large {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas:
+    'aside header'
+    'aside main'
+    'aside footer';
   min-height: 100vh;
-  display: flex;
-  flex-direction: column;
 }
 
 header {
   display: flex;
-  justify-content: space-between;
+  widows: 100vw;
+  justify-content: flex-end;
   padding: 1.5rem;
-  border-bottom: #eeeeee 2px solid;
-  background-color: white;
+  grid-area: header;
+}
 
-  z-index: 10;
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 5%;
+aside {
+  border-right: #eeeeee 1px solid;
+  box-shadow: 0 0 10px 0 #eeeeee;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+  grid-area: aside;
 }
 
 main {
-  margin-top: 5%;
-  margin-bottom: 4rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  padding-bottom: 4rem;
-  padding-top: 1.5rem;
-  position: relative;
+  min-height: 80%;
+  padding: 2rem;
+  grid-area: main;
 }
 
 footer {
-  border-top: #eeeeee 1px solid;
-  box-shadow: 0 0 10px 0 #eeeeee;
-  padding: 1.5rem;
-  margin-top: auto;
-  background-color: white;
-
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  color: white;
-  text-align: center;
-  z-index: 10;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 3rem;
+  background-color: #eeeeee;
+  grid-area: footer;
 }
 </style>
